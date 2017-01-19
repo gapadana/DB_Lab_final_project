94e1256ceb1d5d3c36dd46db559772faedf9f6b0
Ali Ebrahiminejad <a.ebrahiminejad2@gmail.com>
2017-01-20 00:09:00 +0330

    state essential parts added!

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -54,11 +54,12 @@
                     <div class="panel-body">
                         <table class="table table-striped task-table">
                             <thead>
-                                <th>Task</th>
+                                <th>ToDo Task</th>
                                 <th>&nbsp;</th>
                             </thead>
                             <tbody>
                                 @foreach ($tasks as $task)
+				@if ($task->state=="todo")
 				<form action="{{url('tasks/edit/' . $task->id)}}" method="POST">
  				{{ csrf_field() }}
                                 {{ method_field('POST') }}
@@ -67,7 +68,7 @@
 					<td><input type="text" name="description" id="task-description" class="form-control" value="{{ $task->description }}"></td>
 				    </tr>
 				    <tr>
-                                        <td>
+                                        <td colspan=2>
                                             <button type="submit" id="edit-description-{{ $task->id }}" class="btn btn-warning">
                                                 <i class="fa fa-btn fa-edit"></i>Change Description
                                             </button>
@@ -75,7 +76,7 @@
 				    </tr>
                                 </form>
 					<tr>
-						<td>
+						<td colspan=2>
                                         <!-- Task Delete Button -->
 						<form action="{{url('task/' . $task->id)}}" method="POST">
                                                 {{ csrf_field() }}
@@ -87,6 +88,93 @@
                                             </form>
 						</td>
 					</tr>
+				@endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+			</br>
+			<div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th>Doing Task</th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($tasks as $task)
+				@if ($task->state=="doing")
+				<form action="{{url('tasks/edit/' . $task->id)}}" method="POST">
+ 				{{ csrf_field() }}
+                                {{ method_field('POST') }}
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+					<td><input type="text" name="description" id="task-description" class="form-control" value="{{ $task->description }}"></td>
+				    </tr>
+				    <tr>
+                                        <td colspan=2>
+                                            <button type="submit" id="edit-description-{{ $task->id }}" class="btn btn-warning">
+                                                <i class="fa fa-btn fa-edit"></i>Change Description
+                                            </button>
+                                        </td>
+				    </tr>
+                                </form>
+					<tr>
+						<td colspan=2>
+                                        <!-- Task Delete Button -->
+						<form action="{{url('task/' . $task->id)}}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+						</td>
+					</tr>
+				@endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+		</br>
+		<div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th>Done Task</th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($tasks as $task)
+				@if ($task->state=="done")
+				<form action="{{url('tasks/edit/' . $task->id)}}" method="POST">
+ 				{{ csrf_field() }}
+                                {{ method_field('POST') }}
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+					<td><input type="text" name="description" id="task-description" class="form-control" value="{{ $task->description }}"></td>
+				    </tr>
+				    <tr>
+                                        <td colspan=2>
+                                            <button type="submit" id="edit-description-{{ $task->id }}" class="btn btn-warning">
+                                                <i class="fa fa-btn fa-edit"></i>Change Description
+                                            </button>
+                                        </td>
+				    </tr>
+                                </form>
+					<tr>
+						<td colspan=2>
+                                        <!-- Task Delete Button -->
+						<form action="{{url('task/' . $task->id)}}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+						</td>
+					</tr>
+				@endif
                                 @endforeach
                             </tbody>
                         </table>
